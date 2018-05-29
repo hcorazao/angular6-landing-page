723ff54a24a477b5e42468c0cd6f72a0feaa5781
s6348798 <Herbert.Corazao@scotiabank.com.pe>
2018-05-29 15:53:03 -0500

    refactor(money-exchange): lint errors fixed

--- src/app/landing-page/money-exchange/money-exchange.component.ts
+++ src/app/landing-page/money-exchange/money-exchange.component.ts
@@ -51,7 +51,7 @@ export class MoneyExchangeComponent implements OnInit {
     if (!!this.moneyExchangeForm.value.dollarCurrencyInput) {
       this.landingPageService.getCurrency().subscribe(response => {
         const dollarInputValue = this.convertToNumber(this.moneyExchangeForm.value.dollarCurrencyInput);
-        const euroConversion = dollarInputValue*response.rates.EUR;
+        const euroConversion = dollarInputValue * response.rates.EUR;
         this.moneyExchangeForm.controls['euroCurrencyInput'].setValue(euroConversion);
       });
     }
