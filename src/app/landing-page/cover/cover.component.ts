5441015ba8ee0c9271459e63cf978b7cde925874
s6348798 <Herbert.Corazao@scotiabank.com.pe>
2018-05-30 15:41:18 -0500

    refactor(landing-page): unnecesarry oninit function removed

--- src/app/landing-page/cover/cover.component.ts
+++ src/app/landing-page/cover/cover.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cover',
   templateUrl: './cover.component.html',
   styleUrls: ['./cover.component.scss']
 })
-export class CoverComponent implements OnInit {
+export class CoverComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
