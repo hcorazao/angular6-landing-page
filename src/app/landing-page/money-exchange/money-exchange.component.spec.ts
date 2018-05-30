1cbe966ffe15eaa0d866face268abd0c82ace0ed
s6348798 <Herbert.Corazao@scotiabank.com.pe>
2018-05-30 16:03:15 -0500

    feat(money-exchange): initial unit test added

--- src/app/landing-page/money-exchange/money-exchange.component.spec.ts
+++ src/app/landing-page/money-exchange/money-exchange.component.spec.ts
@@ -1,14 +1,51 @@
+// Angular
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+// Modules
+import { TextMaskModule } from 'angular2-text-mask';
+
+// Components
 import { MoneyExchangeComponent } from './money-exchange.component';
+import { CoverComponent } from '../cover/cover.component';
+import { DividerComponent } from '../divider/divider.component';
+import { FooterComponent } from '../footer/footer.component';
+import { LoadingIconComponent } from '../loading-icon/loading-icon.component';
 
-describe('MoneyExchangeComponent', () => {
+// Services
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { LandingPageService } from '../landing-page.service';
+
+fdescribe('MoneyExchangeComponent', () => {
   let component: MoneyExchangeComponent;
   let fixture: ComponentFixture<MoneyExchangeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MoneyExchangeComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        FormsModule,
+        TextMaskModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        MoneyExchangeComponent,
+        CoverComponent,
+        DividerComponent,
+        FooterComponent,
+        LoadingIconComponent
+      ],
+      providers: [
+        LandingPageService
+      ]
     })
     .compileComponents();
   }));
